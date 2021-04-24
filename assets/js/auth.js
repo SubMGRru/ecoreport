1c71d9855515812866a0599f51f0e083163e668e
Aram Virabyan <googlaram200@gmail.com>
2021-04-25 02:07:08 +0300

    Ну что, авторизация вроде есть, надо протестировать хахахаха
    хахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахахаха

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,29 @@
 function lets_start_auth(){
 	console.log('processing.');
 	$.post('https://api.sllc.cloud/ecoreport/isRegistered.php', {email: $("#email").val()}, function(data){
-	alert(data);
+	localStorage.setItem('email', $("#email").val());
+	if(data == 1){
+		$("#login").hide();
+		$("#loginpassword").show();
+		localStorage.setItem('auth_mode', data);
+	}else if(data == 0){
+		$("#login").hide();
+		$("#regpassword").show();
+		localStorage.setItem('auth_mode', data);
+	}
+});
+}
+
+function login(){
+	console.log('signing in.');
+	$.ajax({
+	url:'https://api.sllc.cloud/main/login.php',
+    type:'post',
+    xhrFields: { withCredentials: true },
+    crossDomain: true,
+    data:{email:localStorage.getItem("email"),password:$("#password").val()},
+	success: function(data){
+		alert(data);
+	}
 });
 }
