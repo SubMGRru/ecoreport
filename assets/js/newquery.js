254fcfc79c17f03bedaf53de19ec80591a7e79c1
Aram Virabyan <googlaram200@gmail.com>
2021-04-25 08:53:18 +0300

    [АССИНХРОННАЯ ЗАГРУЗКА]внедрение функции загрузки фотографий инцидентов

--- assets/js/newquery.js
+++ assets/js/newquery.js
@@ -1,4 +1,6 @@
 $("#blocked").hide();
+$("#queryform").hide();
+$("#about_incident").hide();
 navigator.geolocation.getCurrentPosition(success, error, {
   // высокая точность
   enableHighAccuracy: true
@@ -12,6 +14,9 @@ function success({ coords }) {
   tata.success('Получено', 'Теперь можно прикрепить изображение', {
 			  duration: 5000
 			});
+  $("#waiting").hide();
+  $("#queryform").show();
+  
 }
 
 function error({ message }) {
@@ -20,3 +25,31 @@ function error({ message }) {
   $("#blocked").show();
 
 }
+
+$(document).ready(function(){
+  //You might want to do if check to see if localstorage set for theImage here
+  var img = new Image();                
+  img.src = localStorage.theImage;
+
+  $('.imagearea').html(img);
+
+  $("body").on("change",".classhere",function(){
+      //Equivalent of getElementById
+      var fileInput = $(this)[0];//returns a HTML DOM object by putting the [0] since it's really an associative array.
+      var file = fileInput.files[0]; //there is only '1' file since they are not multiple type.
+
+      var reader = new FileReader();
+      reader.onload = function(e) {
+           // Create a new image.
+           var img = new Image();
+
+           img.src = reader.result;
+           localStorage.theImage = reader.result; //stores the image to localStorage
+           $(".imagearea").html(img);
+           $("#letsupload").hide();
+           $("#about_incident").show();
+       }
+
+       reader.readAsDataURL(file);//attempts to read the file in question.
+    });
+});
