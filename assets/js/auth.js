c79cbb0dec83c26e4d4c6d5608c0a2d9a2da50cd
Aram Virabyan <googlaram200@gmail.com>
2021-04-25 11:24:53 +0300

    импелементация регистрации

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,4 +1,6 @@
 $("#loginpassword").hide();
+$("#regname").hide();
+$("#reglastname").hide();
 $("#regpassword").hide();
 function lets_start_auth(){
 	console.log('processing.');
@@ -10,7 +12,7 @@ function lets_start_auth(){
 		localStorage.setItem('auth_mode', data);
 	}else if(data == 0){
 		$("#login").hide();
-		$("#regpassword").show();
+		$("#regname").show();
 		localStorage.setItem('auth_mode', data);
 	}
 });
@@ -43,4 +45,56 @@ function login(){
 
 if(localStorage.getItem("auth") == 'loggedin'){
 	window.location.href = "app.html";
+}
+
+function show_reglastname(){
+	if($("#firstname").val() != ''){
+		$("#regname").hide();
+		$("#reglastname").show();
+	}else{
+		tata.error('Ошибка', 'Введите свое имя, чтобы продолжить', {
+			  duration: 5000
+			});
+	}
+}
+
+function show_reglastname(){
+	if($("#lastname").val() != ''){
+		$("#reglastname").hide();
+		$("#regpassword").show();
+	}else{
+		tata.error('Ошибка', 'Введите свою фамилию, чтобы продолжить', {
+			  duration: 5000
+			});
+	}
+}
+
+function register(){
+	if($("#regpassword").val() != ''){
+		$.ajax({
+			url:'https://api.sllc.cloud/ecoreport/register.php',
+		    type:'post',
+		    xhrFields: { withCredentials: true },
+		    crossDomain: true,
+		    data:{firstname:localStorage.getItem("firstname"),lastname:$("#lastname").val(),email:localStorage.getItem("email"),password:$("#regpassword").val(),passwordConfirm:$("#regpassword").val()},
+			success: function(data){
+				if(data == 1 || data == 2){
+					tata.success('Успешно!', 'Вы вошли в свою учетную запись', {
+					  duration: 5000
+					});
+				localStorage.setItem('auth', 'loggedin');
+				window.location.href = "app.html";
+				}else{
+					console.log(data);
+					tata.error('Ошибка', 'Неправильный пароль, попробуйте еще раз', {
+					  duration: 5000
+					});
+				}
+			}
+		});
+	}else{
+		tata.error('Ошибка', 'Придумайте пароль, чтобы продолжить', {
+			  duration: 5000
+			});
+	}
 }
